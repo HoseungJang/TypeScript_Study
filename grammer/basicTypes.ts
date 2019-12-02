0636788b1dd7cbec2d69fa4ad5d0ed64fceca94a
HoseungJang <hsjang.dev@gmail.com>
2019-12-02 13:52:05 +0900

    ADD: basicTypes.ts 추가

---
+++ grammer/basicTypes.ts
@@ -0,0 +1,11 @@
+// 자바스크립트의 타입 지원
+
+//boolean
+const bool: boolean = true;
+
+//number
+const dec: number = 5;
+const hex: number = 0xf00d;
+const bin: number = 0b1010;
+const oct: number = 0o744;
+console.log(dec, hex, bin, oct);
