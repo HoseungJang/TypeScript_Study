1e2d9686814efd5995dc177ed7048919f2df204f
HoseungJang <hsjang.dev@gmail.com>
2019-12-06 17:24:23 +0900

    ADD: 객체 타입 파트 추가

---
+++ ts/객체 타입.ts
@@ -0,0 +1,19 @@
+/*
+객체 타입
+Array
+Tuple
+Function
+생성자
+Class
+Interface
+*/
+
+// array
+// 타입 뒤에 [] 를 붙여서 사용
+let items: number[] = [1, 2, 3];
+
+// tuple
+let x: [string, number];
+x = ["JangHoseung", 18]; // 초기화는 정의한 타입과 매칭되야함
+
+// Function 타입은 6장에서
