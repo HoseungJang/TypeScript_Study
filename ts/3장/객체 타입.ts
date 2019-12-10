23b943801569f2ddf9897f625865f28854dbf719
HoseungJang <hsjang.dev@gmail.com>
2019-12-10 12:06:55 +0900

    ADD: 객체 타입 파트 추가

---
+++ ts/3장/객체 타입.ts
@@ -0,0 +1,22 @@
+/*
+객체 타입
+Array
+Tuple
+Function
+생성자
+Class
+Interface
+*/
+
+// array
+// 타입 뒤에 [] 를 붙여서 사용
+let items: number[] = [1, 2, 3];
+
+// tuple
+// 배열 요소가 n개로 정해질 때, 각 요소별로 타입을 지정한 타입
+let x: [string, number];
+x = ["JangHoseung", 18]; // 초기화는 순서에 맞게 해야함.
+
+// Function 타입은 6장에서
+
+// 생성자 타입은 7장에서
